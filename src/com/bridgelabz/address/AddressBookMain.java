253fc9d83cb0b421dc7f965f2131233fb0683ddc
rojajullapalli <rojajules@gmail.com>
2021-08-20 17:29:33 +0530

    ADDED:Welcome Message

---
+++ src/com/bridgelabz/address/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.address;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book system");
+    }
+}
